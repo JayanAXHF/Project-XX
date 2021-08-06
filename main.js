422fbfda00450fae1119aa7c786cc6c00f0ccd59
JayanAXHF <73993003+JayanAXHF@users.noreply.github.com>
2021-08-06 16:27:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-//js
+//js function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log("🚀 ~ file: main.js ~ line 18 ~ gotPoses ~ if ~ results", results)
+    }
+}
