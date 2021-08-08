47eda5fa432fa16af72d5cd31796c7587783634f
JayanAXHF <73993003+JayanAXHF@users.noreply.github.com>
2021-08-08 12:04:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//js function setup() {
+function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
 
@@ -9,12 +9,36 @@
 }
 
 function draw() {
-    image(video, 0, 0, 600, 500);
+    // image(video, 0, 0, 600, 500);
+
+    background("#3DFFE1");
+    textSize(difference);
+    fill("#000000");
+
+    text('Ghost', 50, 400);
+
 
 }
 
+
+function modelLoaded() {
+
+}
+
+var difference = 0;
+
+
 function gotPoses(results) {
+
     if (results.length > 0) {
-        console.log("🚀 ~ file: main.js ~ line 18 ~ gotPoses ~ if ~ results", results)
+        console.log("file: main.js ~ line 28 ~ gotPoses ~ if ~ results", results)
+
+
+        leftwristX = results[0].pose.leftWrist.x;
+        rightwristX = results[0].pose.rightWrist.x;
+
+
+
+        difference = floor(leftwristX - rightwristX);
     }
 }
